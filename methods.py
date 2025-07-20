935409b5dad4af24fa4d334d9812d6747501646f
SURNI KURMA RAO <kurmaraosurni@gmail.com>
2025-07-20 21:25:00 +0530

    Create methods.py

---
+++ methods.py
@@ -0,0 +1,10 @@
+# #method:A method is a function that belongs to an object — specifically, an object created from a class.
+
+# If you define a function inside a class, it's called a method.
+
+# It usually acts on the object’s data (its attributes).
+class Teacher:
+    def action(self):
+        print("teaching")
+o=Teacher()
+o.action()
